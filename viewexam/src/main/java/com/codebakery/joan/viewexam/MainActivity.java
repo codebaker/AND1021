4becd8e61599d9c1a825f6a04836880c3ac9e17f
codebaker <jungah.kim76@gmail.com>
2018-10-25 01:04:42 +0900

    first

---
+++ viewexam/src/main/java/com/codebakery/joan/viewexam/MainActivity.java
@@ -0,0 +1,17 @@
+package com.codebakery.joan.viewexam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.hello_text);
+        textView.setText("안녕하세요.");
+    }
+}
